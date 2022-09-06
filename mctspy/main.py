e07c6bc90e47b6f1f31b292adc587298bd0a156a
Paul Adamowicz <p.adamowicz18@ejm.org>
2022-09-06 17:46:11 +0200

    A2C + MCTS implementation guidelines

--- mctspy/main.py
+++ mctspy/main.py
@@ -1,3 +1,4 @@
+import collections
 import numpy as np
 import random
 
@@ -41,3 +42,26 @@ for _ in range(10000):
     res.append(r)
     print('Game Over \n\n')
 print(f'wins{res.count(1)} losses{res.count(-1)} draws{res.count(0)}')
+
+
+
+def train():
+
+    # 1. Init a buffer that contains 10000 positions (board, action, value) & INIT MCTS + NN agents
+    BUFFER = collections.deque(maxlen=10000)
+    NN = A2CAgent()
+    MCTS = MCTSAgent(NN) # TODO init MCTS agent with a neural network
+
+    # 2. Call play to generate positions using self.play with MCTS (btw. for chess, take max 30 positions by game to avoid overfitting)
+    for _ in range(1000):
+        positions = play() # TODO play() should return a list of positions (board, action, value)
+        BUFFER.append(positions)
+
+    # 3. Train the network using the buffer:
+    for _ in range(200):
+        A2CAgent().train(BUFFER) # TODO Code an agent to handle the training process with NN
+
+    # 4. After 200 training steps, update MCTS with the new NN
+    MCTS.update(NN) # TODO update MCTS with the new NN
+
+    # 5. Start over.
---
+++ mctspy/network.py
@@ -0,0 +1,68 @@
+"""
+Neural Network
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+# from config import CFG
+
+class A2CNet(nn.Module):
+    """
+    A2C Neural Network
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        net = [
+            nn.Conv2d(111, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+        ]
+
+        for _ in range(CFG.convolution_layers - 3):
+            net += [
+                nn.Conv2d(512, 512, kernel_size=3, padding=1),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True),
+            ]
+
+        net += [
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Flatten(start_dim=1),
+            nn.Linear(in_features=16384, out_features=8192),
+            nn.ReLU(inplace=True),
+        ]
+
+        self.net = nn.Sequential(*net)
+
+        self.val = nn.Sequential(
+            nn.Linear(in_features = 8192, out_features = 2048),
+            nn.ReLU(inplace = True),
+            nn.Linear(in_features = 2048, out_features = 256),
+            nn.ReLU(inplace = True),
+            nn.Linear(in_features = 256, out_features = 1),
+            nn.Tanh()
+        )
+
+        self.pol = nn.Sequential(
+            nn.Linear(8192, 8192), nn.ReLU(inplace = True),
+            nn.Linear(8192, 4096), nn.ReLU(inplace = True),
+            nn.Linear(4096, 4672),
+            nn.LogSoftmax(dim=-1),
+        )
+
+
+    def forward(self, X):
+        """
+        Equivalent of a .fit for a neural network in pytorch
+        """
+        y = self.net(X)
+        y_val = self.val(y)
+        y_pol = self.pol(y).double() # We need double precision for small probabilities.
+        # torch.clamp(y_pol, min=10e-39, max=1)
+        return y_val, y_pol.exp()
